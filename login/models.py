9b9dece2884a5170acaeb0397d43e288739b0769
admin <christinadu1d@outlook.com>
2020-12-07 11:57:09 +0800

    蓝色外观+正确的navbar（bootstrap4）

---
+++ login/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class Users(models.Model):
+    """docstring for Users"""
+    # 用户名必填，最长不超过128个字符且唯一
+    name = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=256)
+    email = models.EmailField(unique=True)
+    # create_time 用户被创建的时间
+    c_time = models.DateTimeField(auto_now_add=True)
+
+    # 使用__str__人性化显示对象信息
+    def __str__(self):
+        return self.name
+
+    # 元数据里，定义用户按创建时间的反序排列
+    # aka 最近创建的最先显示
+    class Meta:
+        ordering = ['c_time']
+        verbose_name = '用户'
+        verbose_name_plural = '用户'
---
+++ login/views.py
@@ -0,0 +1,22 @@
+# login/views.py
+
+from django.shortcuts import render,redirect
+
+def index(request):
+    pass
+    return render(request,'login/index.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+    return render(request, 'login/login.html')
+
+
+def register(request):
+    pass
+    return render(request,'login/register.html')
+
+def logout(request):
+    pass
+    return redirect('/index/')
