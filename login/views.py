c3642d64d3955a14eab1115b47e3531284afe60e
admin <christinadu1d@outlook.com>
2020-12-09 16:25:10 +0800

    使用hashlib添加加密

--- login/views.py
+++ login/views.py
@@ -1,7 +1,8 @@
 # login/views.py
 from django.shortcuts import render, redirect
 from login.models import Users
-from .forms import UserForm
+from .forms import UserForm, RegisterForm
+import hashlib
 
 
 def index(request):
@@ -23,7 +24,7 @@ def login(request):
             # 更多的其它验证.....
             try:
                 user = Users.objects.get(name=username)
-                if user.password == password:
+                if user.password == hash_code(password):
                     request.session['is_login'] = True
                     request.session['user_id'] = user.id
                     request.session['user_name'] = user.name
@@ -38,8 +39,40 @@ def login(request):
 
 
 def register(request):
-    pass
-    return render(request, 'login/register.html')
+    if request.session.get('is_login', None):
+        # 登录状态不允许注册
+        return redirect("/index/")
+    if request.method == "POST":
+        register_form = RegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():  # 获取数据
+            username = register_form.cleaned_data['username']
+            password1 = register_form.cleaned_data['password1']
+            password2 = register_form.cleaned_data['password2']
+            email = register_form.cleaned_data['email']
+            if password1 != password2:  # 判断两次密码是否相同
+                message = "两次输入的密码不同！"
+                return render(request, 'login/register.html', locals())
+            else:
+                same_name_user = Users.objects.filter(name=username)
+                if same_name_user:  # 用户名唯一
+                    message = '用户已经存在，请重新选择用户名！'
+                    return render(request, 'login/register.html', locals())
+                same_email_user = Users.objects.filter(email=email)
+                if same_email_user:  # 邮箱地址唯一
+                    message = '该邮箱地址已被注册，请使用别的邮箱！'
+                    return render(request, 'login/register.html', locals())
+
+                # 当一切都OK的情况下，创建新用户
+
+                new_user = Users.objects.create()
+                new_user.name = username
+                new_user.password = hash_code(password1)
+                new_user.email = email
+                new_user.save()
+                return redirect('/login/')  # 自动跳转到登录页面
+    register_form = RegisterForm()
+    return render(request, 'login/register.html', locals())
 
 
 def logout(request):
@@ -47,3 +80,10 @@ def logout(request):
     #     return redirect('/login/#')
     request.session.flush()
     return redirect('/login/login')
+
+
+def hash_code(s, salt='overcloud'):  # 加点盐
+    h = hashlib.sha256()  # 使用 sha256() 创建一个SHA-256 hash 对象
+    s += salt
+    h.update(s.encode())  # update方法只接收bytes类型
+    return h.hexdigest()
